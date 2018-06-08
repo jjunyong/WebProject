c0cffb6a8a033ae9440bff6cd16002b133673bc6
Hamin Choi <21300786@handong.edu>
2018-06-08 22:33:57 +0900

    revised

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,7 +10,26 @@ const mailTransport = nodemailer.createTransport(
 const APP_NAME = '한동 매칭';
 
 //매치 성사시 신청한 사람에게 이메일
+// export const matchComplteEmail =
+//     functions.firestore.document(`matches/{MatchId}/isMatched`)
+//         .onUpdate((snap, context) => {
+//             const match = snap.after.data();
+//             const user = admin.firestore().collection('users').doc(match.away_id).
+           
 
+//             const mailOptions = {
+//                 from: `${APP_NAME} <hdaution@gmail.com>`,
+//                 to: user.email,
+//                 subject: `New challenge to your team!`,
+//                 text: ` ${user.displayName}! 당신의 팀에 새로운 도전자가 등장하였습니다.
+//                 접속하여 수락 또는 거부하여주세요.
+//                         -from ${APP_NAME}.`
+//             };
+
+//             return mailTransport.sendMail(mailOptions).then(() => {
+//                 console.log(`matchRequestEmail sent to ${user.email}`);
+//             });
+//         });
 
 //매치 신청시 호스트에게 이메일
 export const matchRequestEmail =
